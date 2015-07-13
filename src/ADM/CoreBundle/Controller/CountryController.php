ca8258eb55dda4f4fbbd0c82ef9fe1b1d3fd57e9
LehouxHugo <hugo.lehoux@gmail.com>
2015-07-13 11:13:10 +0200

    Retrieved national Data from FAO API

---
+++ src/ADM/CoreBundle/Controller/CountryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ADM\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ADM\CoreBundle\Entity\Country;
+
+class CountryController extends Controller
+{
+    public function readAction(Country $country)
+    {
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+
+        $countryRepository = $em->getRepository('ADMCoreBundle:Country');
+
+        $url="http://data.fao.org/developers/api/v1/fr/resources/faostat/crop-prod/facts.json?page=1&pageSize=200&filter=cnt.iso3%20eq%20BRA&fields=cnt.iso3%20AS%20iso3%2C%20year%2C%20item%20AS%20crop%2C%20m5419%20as%20Production";
+        $json = file_get_contents($url);
+        $json = json_decode($json, true);
+        $json = $json['result'];
+        $json = $json['list'];
+        $json = $json['items'];
+        $FAOdata = json_encode($json);
+
+
+        return $this->render(
+            'ADMCoreBundle:Country:read.html.twig',
+            array(
+                'country' => $country,
+                'FAOdata' => $FAOdata
+            )
+        );
+    }
+
+}
