b47e4aa0317e36eadf29607a88b64b64e3db5aea
LehouxHugo <hugo.lehoux@gmail.com>
2015-07-16 10:32:43 +0200

    Added FAO data to country profiles

--- src/ADM/CoreBundle/Controller/CountryController.php
+++ src/ADM/CoreBundle/Controller/CountryController.php
@@ -15,20 +15,28 @@ class CountryController extends Controller
 
         $countryRepository = $em->getRepository('ADMCoreBundle:Country');
 
-        $url="http://data.fao.org/developers/api/v1/fr/resources/faostat/crop-prod/facts.json?page=1&pageSize=500&filter=cnt.iso3%20eq%20BRA&fields=cnt.iso3%20AS%20iso3%2C%20year%2C%20item%20AS%20crop%2C%20m5419%20as%20Production";
+        $url="http://data.fao.org/developers/api/v1/fr/resources/faostat/crop-prod/facts.json?page=1&pageSize=15000&filter=cnt.iso3%20eq%20BRA&fields=cnt.iso3%20AS%20iso3%2C%20year%2C%20item%20AS%20crop%2C%20m5419%20as%20Production";
         $json = file_get_contents($url);
         $json = json_decode($json, true);
         $json = $json['result'];
         $json = $json['list'];
         $json = $json['items'];
-        $FAOdata = json_encode($json);
-
+        foreach($json as $item){$item['Production'] = round($item['Production'],-2);        };
+        $json = json_encode($json);
+        $json = preg_replace("/\{/", "[",$json);
+        $json = preg_replace("/\}/", "]",$json);
+        $json = preg_replace("/\"area\"\:/", "",$json);
+        $json = preg_replace("/\"Item\"\:/", "",$json);
+        $json = preg_replace("/\"Production\"\:/", "",$json);
+        $json = preg_replace("/\"iso3\"\:/", "",$json);
+        $json = preg_replace("/\"year\"\:/", "",$json);
+        $FAOData = preg_replace('/\[/', '[["area","Item","Production","iso3","year"],', $json, 1);
 
         return $this->render(
             'ADMCoreBundle:Country:read.html.twig',
             array(
                 'country' => $country,
-                'FAOdata' => $FAOdata
+                'FAOdata' => $FAOData
             )
         );
     }
