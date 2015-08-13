a1c5382c8b8922892c50558def1bb94b1e6b863d
LehouxHugo <hugo.lehoux@gmail.com>
2015-08-13 17:42:44 +0200

    Modified Index

--- src/ADM/CoreBundle/Controller/CoreController.php
+++ src/ADM/CoreBundle/Controller/CoreController.php
@@ -4,12 +4,24 @@ namespace ADM\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ADM\CoreBundle\Entity\Country;
+use ADM\ReportsBundle\Entity\Report;
 
 class CoreController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ADMCoreBundle:Core:index.html.twig');
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('ADMReportsBundle:Report');
+
+        $listReports = $repository->findAll();
+
+        return $this->render('ADMCoreBundle:Core:index.html.twig',
+            array(
+                'listReports' => $listReports,
+            )
+        );
+
     }
 
     public function testBasicLayoutAction()
